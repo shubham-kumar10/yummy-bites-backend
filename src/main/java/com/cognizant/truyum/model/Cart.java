c606200c8e924034cbca50767bf15b89e539d346
Kumar, Shubham <shubham.snu@gmail.com>
2020-02-27 21:58:38 +0530

    interface

---
+++ src/main/java/com/cognizant/truyum/model/Cart.java
@@ -0,0 +1,63 @@
+package com.cognizant.truyum.model;
+
+import java.util.List;
+
+public class Cart {
+
+	private List<MenuItem> menuItemList;
+	private double total;
+
+	// Constructors
+	public Cart(List<MenuItem> menuItemList, double total) {
+		super();
+		this.menuItemList = menuItemList;
+		this.total = total;
+	}
+
+	// Getters and Setters
+	public List<MenuItem> getMenuItemList() {
+		return menuItemList;
+	}
+
+	public void setMenuItemList(List<MenuItem> menuItemList) {
+		this.menuItemList = menuItemList;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(total);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cart other = (Cart) obj;
+		if (Double.doubleToLongBits(total) != Double.doubleToLongBits(other.total))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Cart[" +menuItemList+" "+total +"]";
+	}
+
+}
